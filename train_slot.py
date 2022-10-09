adcfa617fc10ece7493064f396d12751fbf365c6
leo922oel <leo922oel@gmail.com>
2022-10-10 01:09:30 +0800

    impl. ver.1

--- train_slot.py
+++ train_slot.py
@@ -1,5 +1,7 @@
 import json
 import pickle
+import numpy as np
+import random
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
 from typing import Dict
@@ -11,16 +13,110 @@ from tqdm import tqdm, trange
 
 from dataset import SeqTaggingClsDataset
 from model import SeqTagger
-from utils import Vocab
+from utils import Vocab, CEMetrics, SlotMetrics
 
 TRAIN = "train"
 DEV = "eval"
 SPLITS = [TRAIN, DEV]
 
 
+def train_epoch(args, model, dataloader, optim):
+    model.train()
+    ce = CEMetrics()
+    acc = SlotMetrics()
+
+    bar = tqdm(dataloader)
+    for i, batch in enumerate(bar):
+        batch["tokens"] = batch["tokens"].to(args.device)
+        batch["tags"] = batch["tags"].to(args.device)
+        batch["mask"] = batch["mask"].to(args.device)
+
+        optim.zero_grad()
+        output = model(batch)
+
+        bar.set_postfix(loss=output["loss"].item(), iter=i, lr=optim.param_groups[0]["lr"])
+
+        ce.udpate(output["loss"], batch["tokens"].size(0))
+        acc.update(batch["tags"].detach().cpu(), output["pred_labels"].detach().cpu(), batch["mask"].cpu())
+        loss = output["loss"]
+
+        loss.backward()
+        optim.step()
+
+    acc.eval()
+    print("Train loss: {:6.4f}\t Joint Acc: {:6.4f}\t Token Acc: {:6.4f}".format(ce.avg, acc.joi_acc, acc.tok_acc))
+    return ce.avg, acc.joi_acc, acc.tok_acc
+
+@torch.no_grad()
+def validation(args, model, dataloader):
+    model.eval()
+    ce = CEMetrics()
+    acc = SlotMetrics()
+
+    for batch in dataloader:
+        batch["tokens"] = batch["tokens"].to(args.device)
+        batch["tags"] = batch["tags"].to(args.device)
+        batch["mask"] = batch["mask"].to(args.device)
+
+        output = model(batch)
+
+        ce.udpate(output["loss"], batch["tokens"].size(0))
+        acc.update(batch["tags"].cpu(), output["pred_labels"].cpu(), batch["mask"].cpu())
+
+    acc.eval()
+    print("Train loss: {:6.4f}\t Joint Acc: {:6.4f}\t Token Acc: {:6.4f}".format(ce.avg, acc.joi_acc, acc.tok_acc))
+    return ce.avg, acc.joi_acc, acc.tok_acc
+
 def main(args):
-    # TODO: implement main function
-    raise NotImplementedError
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+    random.seed(args.seed)
+
+    with open(args.cache_dir / "vocab.pkl", "rb") as f:
+        vocab: Vocab = pickle.load(f)
+
+    ckpt_dir = f"{args.checkpt_dir}/{args.name}"
+    ckpt_dir.mkdir(parents=True, exist_ok=True)
+    tag_idx_path = args.cache_dir / "tag2idx.json"
+    tag_idx_path.mkdir(parents=True, exist_ok=True)
+    tag2idx: Dict[str, int] = json.loads(tag_idx_path.read_text())
+
+    data_paths = {split: args.data_dir / f"{split}.json" for split in SPLITS}
+    data_paths.mkdir(parents=True, exist_ok=True)
+    data = {split: json.loads(path.read_text()) for split, path in data_paths.items()}
+    datasets: Dict[str, SeqTaggingClsDataset] = {
+        split: SeqTaggingClsDataset(split_data, vocab, tag2idx, args.max_len)
+        for split, split_data in data.items()
+    }
+
+    dataloader: Dict[str, DataLoader] = {
+        split: DataLoader(split_data, args.batch_size, shuffle=True, collate_fn=split_data.collate_fn)
+        for split, split_data in datasets.items()
+    }
+
+    embeddings = torch.load(args.cache_dir / "embeddings.pt")
+    # TODO: init model and move model to target device(cpu / gpu)
+    model = SeqTagger(embeddings, args.hidden_size, args.num_layers,
+            args.dropout, args.bidirectional, datasets[TRAIN].num_classes,).to(args.device)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+    # optimizer = torch.optim.SGD(model.parameters(), lr=args.lr)
+    # optimizer = torch.optim.Adagrad(model.parameters(), lr=args.lr)
+
+    best_acc = 0.0
+    epoch_pbar = trange(args.num_epoch, desc="Epoch")
+    for epoch in epoch_pbar:
+        print(f"Epoch: {epoch}")
+        train_ce, train_joi_acc, train_tok_acc = train_epoch(args, model, dataloader[TRAIN], optimizer, )
+        val_ce, val_joi_acc, val_tok_acc = validation(args, model, dataloader[DEV])
+
+        ckpt_path = f"{ckpt_dir}/{epoch+1}-model.pth"
+        best_path = f"{ckpt_dir}/best-model.pth"
+        torch.save(model.state_dict(), ckpt_path)
+        if val_joi_acc > best_acc:
+            best_acc = val_joi_acc
+            torch.save(model.state_dict(), best_path)
+            print(f"Save model checkpoint -- {best_path}")
 
 
 def parse_args() -> Namespace:
@@ -64,6 +160,7 @@ def parse_args() -> Namespace:
         "--device", type=torch.device, help="cpu, cuda, cuda:0, cuda:1", default="cpu"
     )
     parser.add_argument("--num_epoch", type=int, default=100)
+    parser.add_argument("--seed", type=int, default=1000, help="seed for training")
 
     args = parser.parse_args()
     return args
