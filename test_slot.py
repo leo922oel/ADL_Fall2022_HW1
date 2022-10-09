12464f79c83df22580180fea39240d9c5c0570b9
leo922oel <leo922oel@gmail.com>
2022-10-10 01:25:20 +0800

    impl. ver.1

--- test_slot.py
+++ test_slot.py
@@ -1,5 +1,7 @@
 import json
 import pickle
+import numpy as np
+import random
 from argparse import ArgumentParser, Namespace
 from pathlib import Path
 from typing import Dict
@@ -14,9 +16,64 @@ from utils import Vocab
 
 
 def main(args):
-    # TODO: implement main function
-    raise NotImplementedError
+    torch.manual_seed(args.seed)
+    np.random.seed(args.seed)
+    random.seed(args.seed)
 
+    with open(args.cache_dir / "vocab.pkl", "rb") as f:
+        vocab: Vocab = pickle.load(f)
+
+    slot_idx_path = args.cache_dir / "tag2idx.json"
+    slot_idx_path.mkdir(parents=True, exist_ok=True)
+    tag2idx: Dict[str, int] = json.loads(slot_idx_path.read_text())
+
+    data = json.loads(args.test_file.read_text())
+    dataset = SeqTaggingClsDataset(data, vocab, tag2idx, args.max_len)
+    dataloader = DataLoader(dataset, args.batch_size, shuffle=False, collate_fn=dataset.collate_fn)
+
+    embeddings = torch.load(args.cache_dir / "embeddings.pt")
+
+    model = SeqTagger(
+        embeddings,
+        args.hidden_size,
+        args.num_layers,
+        args.dropout,
+        args.bidirectional,
+        dataset.num_classes,
+    ).to(args.device)
+    model.eval()
+
+    ckpt = torch.load(args.ckpt_path)
+    # load weights into model
+    model.load_state_dict(ckpt)
+
+    ids = []
+    tags = []
+    lens = []
+
+    for batch in dataloader:
+        batch["tokens"] = batch["tokens"].to(args.device)
+        batch["tags"] = batch["tags"].to(args.device)
+        batch["mask"] = batch["mask"].to(args.device)
+
+        with torch.no_grad():
+            output = model(batch)
+        ids += batch["id"]
+        tags += output["pred_labels"].cpu().tolist()
+        lens += batch["mask"].sum(-1).long().cpu().tolist()
+
+    if args.pred_file.parent:
+        args.pred_file.parent.mkdir(parent=True, exist_ok=True)
+    with open(args.pred_file, 'w') as f:
+        f.write('id,tags\n')
+        for id, tag, len in zip(ids, tags, lens):
+            f.write("%s," %(id))
+            for idx, t in enumerate(tag):
+                if idx < len-1:
+                    f.write("%s " %(dataset.idx2label(t)))
+                else:
+                    f.write("%s\n" %(dataset.idx2label(t)))
+                    break
 
 def parse_args() -> Namespace:
     parser = ArgumentParser()
@@ -55,6 +112,7 @@ def parse_args() -> Namespace:
     parser.add_argument(
         "--device", type=torch.device, help="cpu, cuda, cuda:0, cuda:1", default="cpu"
     )
+    parser.add_argument("--seed", type=int, default=1000, help="seed for testing")
     args = parser.parse_args()
     return args
 
