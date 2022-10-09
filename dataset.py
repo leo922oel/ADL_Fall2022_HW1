376367eeeb1dcc9d6fcc579a12e53d8c517c2595
leo922oel <leo922oel@gmail.com>
2022-10-09 20:04:36 +0800

    dataset impl. ver.1

--- dataset.py
+++ dataset.py
@@ -2,9 +2,8 @@ from typing import List, Dict
 
 import torch
 from torch.utils.data import Dataset
-from torch.nn.utils.rnn import pad_sequence
 
-from utils import Vocab
+from utils import Vocab, pad_to_len
 
 
 class SeqClsDataset(Dataset):
@@ -34,15 +33,22 @@ class SeqClsDataset(Dataset):
 
     def collate_fn(self, samples: List[Dict]) -> Dict:
         # TODO: implement collate_fn
-        text = [torch.tensor(s["text"]) for s in samples]
-        intent = [torch.tensor(s["intent"]) for s in samples]
-        text = pad_sequence(text, batch_first=True)
+        sorted_samples = samples.sort(key=lambda s: len(s["text"].split()), )
+        batch = {}
+        batch["text"] = [s["text"].split() for s in sorted_samples]
+        batch["len"] = torch.tensor([min(len(s), self.max_len) for s in batch["text"]])
+        batch["text"] = torch.tensor(
+            self.vocab.encode_batch(batch["text"], self.max_len)
+            )
+        batch["id"] = torch.tensor([s["id"] for s in sorted_samples])
+        if "intent" in sorted_samples.keys():
+            batch["intent"] = torch.tensor(
+                self.label2idx(s["intent"] for s in sorted_samples)
+            )
+        else: 
+            batch["intent"] = torch.zeros(len(sorted_samples), dtype=torch.uint8)
 
-        # raise NotImplementedError
-        return {
-            'text': text,
-            'intent': intent
-            }
+        return batch
 
     def label2idx(self, label: str):
         return self.label_mapping[label]
@@ -54,8 +60,30 @@ class SeqClsDataset(Dataset):
 class SeqTaggingClsDataset(SeqClsDataset):
     ignore_idx = -100
 
+    def count_label(self):
+        self.count = torch.zeros(len(self.label_mapping))
+        if "tags" in self.data:
+            for data in self.data:
+                for tag in data["tags"]:
+                    self.count[self.label2idx(tag)] += 1
+        else: self.count += 1
+
     def collate_fn(self, samples):
         # TODO: implement collate_fn
-        tokens = [s["tokens"] for s in samples]
-        tags = [s["tags"] for s in samples]
-        raise NotImplementedError
+        sorted_samples = samples.sort(key=lambda s: len(s["tokens"]), )
+        batch = {}
+        batch["tokens"] = [s["tokens"] for s in sorted_samples]
+        batch["len"] = torch.tensor([min(len(s), self.max_len) for s in batch["tokens"]])
+        batch["tokens"] = torch.tensor(
+            self.vocab.encode_batch(batch["tokens"], self.max_len)
+            )
+        batch["id"] = torch.tensor([s["id"] for s in sorted_samples])
+        if "tags" in sorted_samples.keys():
+            batch["tags"] = [[self.label2idx(t) for t in s["tags"]] for s in sorted_samples]
+            batch["tags"] = pad_to_len(batch["tags"], self.max_len, 0)
+        else: 
+            batch["tags"] = [[0]*self.max_len] * len(sorted_samples)
+        batch["tags"] = torch.tensor(batch["tags"])
+        # batch["mask"] = batch["tokens"].gt(0).float()
+
+        return batch
