67b011d001e2dc6ca5704cd534ada9f7ead7e2f6
leo922oel <leo922oel@gmail.com>
2022-10-10 00:03:18 +0800

    Intent & Slot Metrics impl. ver.1

--- utils.py
+++ utils.py
@@ -42,3 +42,39 @@ class Vocab:
 def pad_to_len(seqs: List[List[int]], to_len: int, padding: int) -> List[List[int]]:
     paddeds = [seq[:to_len] + [padding] * max(0, to_len - len(seq)) for seq in seqs]
     return paddeds
+
+class IntentMetrics(object):
+    def __init__(self, eps=1e-8):
+        self.eps = eps
+        self.correct = 0
+        self.n = 0
+    
+    def update(self, ground, pred):
+        n = ground.size(0)
+        self.correct += pred.eq(ground.view_as(pred)).sum().item()
+        self.n += n
+
+    def eval(self):
+        self.acc = self.correct / (self.n + self.eps)
+
+class SlotMetrics(object):
+    def __init__(self, eps=1e-8):
+        self.eps = eps
+        self.tok_cor = 0
+        self.joi_cor = 0
+        self.tok_n = 0
+        self.joi_n = 0
+    
+    def update(self, ground, pred, mask):
+        mask = mask[:, :ground.size(1)]
+        batch_cor = (pred.eq(ground.view_as(pred)) * mask).sum(-1)
+        len = mask.sum(-1)
+
+        self.tok_cor += batch_cor.sum().long().item()
+        self.joi_cor += batch_cor.eq(len).sum().item()
+        self.tok_n += mask.sum().long().item()
+        self.joi_n += len(ground)
+
+    def eval(self):
+        self.tok_acc = self.tok_cor / (self.tok_n + self.eps)
+        self.joi_acc = self.joi_cor / (self.joi_n + self.eps)
